493c32f6ff91730ffc2694e362c783decb5ce46f
qingzheng cong <114484820+cqz123321@users.noreply.github.com>
2024-08-18 12:58:27 +0100

    Update TransIntegrated.py

--- TransIntegrated.py
+++ TransIntegrated.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from TranMarker import MarkerTracker
-from TranCounter import CameraProcessor
+from Trancontour import CameraProcessor
 
 import cv2
 import numpy as np
